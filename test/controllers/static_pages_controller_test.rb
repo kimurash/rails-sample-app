e217c701c348949ff41422dddefd4bb32eda0eba
shunsei <kimshun1227@icloud.com>
2024-07-31 11:20:47 +0900

    Create about page

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,12 +2,20 @@ require "test_helper"
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
+    # GETリクエストをhomeアクションに対して発行
     get static_pages_home_url
+    # レスポンスは成功するはず
+    # :successはステータスコード200を表す
     assert_response :success
   end
 
   test "should get help" do
     get static_pages_help_url
+    assert_response :success
+  end
+
+  test "should get about" do
+    get static_pages_about_url
     assert_response :success
   end
 end
