ad55258b19db15ec2fe1b62a79deeaae3128bf4b
shunsei <kimshun1227@icloud.com>
2024-08-03 20:29:01 +0900

    Add secure password

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,4 +21,13 @@ class User < ApplicationRecord
       uniqueness: true
     }
   )
+
+  has_secure_password
+  validates(
+    :password,
+    {
+      presence: true,
+      length: { minimum: 6 }
+    }
+  )
 end
