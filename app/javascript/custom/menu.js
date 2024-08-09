5bccbd12eded8c217b74f91b04ba361331f9320f
shunsei <kimshun1227@icloud.com>
2024-08-10 01:35:33 +0900

    Adjust style for mobile

--- app/javascript/custom/menu.js
+++ app/javascript/custom/menu.js
@@ -1,16 +1,17 @@
 // メニュー操作
 
-// トグルリスナーを追加してクリックをリッスンする
-// ページ読み込みイベントにリスナーを追加
+// トグルリスナーを追加する
+function addToggleListener(selected_id, menu_id, toggle_class) {
+    let selected_element = document.querySelector(`#${selected_id}`);
+    selected_element.addEventListener("click", function (event) {
+        event.preventDefault();
+        let menu = document.querySelector(`#${menu_id}`);
+        menu.classList.toggle(toggle_class);
+    });
+}
+
+// クリックをリッスンするトグルリスナーを追加する
 document.addEventListener("turbo:load", function () {
-    let account = document.querySelector("#account");
-    if (account) {
-        // クリックイベントにリスナーを追加
-        account.addEventListener("click", function (event) {
-            // デフォルトのイベントをキャンセルする
-            event.preventDefault();
-            let menu = document.querySelector("#dropdown-menu");
-            menu.classList.toggle("active");
-        });
-    }
+    addToggleListener("hamburger", "navbar-menu", "collapse");
+    addToggleListener("account", "dropdown-menu", "active");
 });
