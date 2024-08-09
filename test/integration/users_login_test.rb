bea9b70468ba7e4a4a2719cbba9148cd0930c70b
shunsei <kimshun1227@icloud.com>
2024-08-10 02:47:45 +0900

    Implement logout

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -1,57 +1,69 @@
 require 'test_helper'
 
-class UsersLoginTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-  #
+class UsersLogin < ActionDispatch::IntegrationTest
   def setup
     @user = users(:michael)
   end
 
-  test 'login with invalid information' do
-    # ログイン用のページにアクセス
+  test 'login path' do
     get login_path
-    # 正しいページが表示されるか確認
     assert_template 'sessions/new'
-    # 無効な情報を用いてPOST
-    post login_path,
-         params: {
-           session: {
-             email: 'michael@example.com',
-             password: 'invalid'
-           }
-         }
-    # 正しいページが再表示されるか確認
+  end
+end
+
+class InvalidPasswordTest < UsersLogin
+  test 'login with valid email/invalid password' do
+    post login_path, params: { session: { email: @user.email,
+                                          password: 'invalid' } }
+    assert_not is_logged_in?
     assert_template 'sessions/new'
-    # flashメッセージが表示されるか確認
     assert_not flash.empty?
-    # 別のページに移動
     get root_path
-    # flashメッセージが消えているか確認
     assert flash.empty?
   end
+end
 
-  test 'login with valid information' do
-    # GETする必要はない
-    # get login_path
-    post login_path,
-         params: {
-           session: {
-             email: @user.email,
-             password: 'password'
-           }
-         }
+class ValidLogin < UsersLogin
+  def setup
+    super
+    post login_path, params: { session: { email: @user.email,
+                                          password: 'password' } }
+  end
+end
 
+class ValidLoginTest < ValidLogin
+  test 'valid login' do
+    assert is_logged_in?
     assert_redirected_to @user
+  end
+
+  test 'redirect after login' do
     follow_redirect!
     assert_template 'users/show'
-
-    # ログインリンクが消えていることを確認
     assert_select 'a[href=?]', login_path, count: 0
-    # ログアウトリンクが表示されていることを確認
     assert_select 'a[href=?]', logout_path
-    # プロフィールリンクが表示されていることを確認
     assert_select 'a[href=?]', user_path(@user)
+  end
+end
+
+class Logout < ValidLogin
+  def setup
+    super
+    delete logout_path
+  end
+end
+
+class LogoutTest < Logout
+  test 'successful logout' do
+    assert_not is_logged_in?
+    assert_response :see_other
+    assert_redirected_to root_url
+  end
+
+  test 'redirect after logout' do
+    follow_redirect!
+    assert_select 'a[href=?]', login_path
+    assert_select 'a[href=?]', logout_path,      count: 0
+    assert_select 'a[href=?]', user_path(@user), count: 0
   end
 end
