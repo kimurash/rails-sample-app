7a7ca7106e57bae06a8aec25798cd3896b9c7f91
shunsei <kimshun1227@icloud.com>
2024-08-10 15:56:58 +0900

    Implement friendly forwarding

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,14 @@
 class SessionsController < ApplicationController
-  def new; end
+  def new
+    debugger
+  end
 
   # rubocop:disable Metrics/AbcSize
+  # rubocop:disable Metrics/MethodLength
   def create
     @user = User.find_by(email: params[:session][:email].downcase)
     if @user&.authenticate(params[:session][:password])
+      forwarding_url = session[:forwarding_url]
       # セッション固定を防ぐために
       # ログインの直前に必ずセッションをリセットする
       reset_session
@@ -12,7 +16,7 @@ class SessionsController < ApplicationController
       params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
       # ログイン後にユーザー情報のページにリダイレクトする
       log_in(@user)
-      redirect_to @user
+      redirect_to forwarding_url || @user
     else
       # エラーメッセージを作成する
       flash.now[:danger] = 'Invalid email/password combination'
--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -36,4 +36,25 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     assert_equal name, @user.name
     assert_equal email, @user.email
   end
+
+  test 'successful edit with friendly forwarding' do
+    get edit_user_path(@user)
+    assert_equal session[:forwarding_url], request.original_url
+
+    log_in_as(@user)
+    assert_redirected_to edit_user_url(@user)
+    assert_nil session[:forwarding_url]
+
+    name = 'Foo Bar'
+    email = 'foo@bar.com'
+    patch user_path(@user), params: { user: { name:,
+                                              email:,
+                                              password: '',
+                                              password_confirmation: '' } }
+    assert_not flash.empty?
+    assert_redirected_to @user
+    @user.reload
+    assert_equal name, @user.name
+    assert_equal email, @user.email
+  end
 end
