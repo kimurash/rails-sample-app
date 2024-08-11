bb017dd021dfd65b7ffe87c1e756b9f7e36fa681
shunsei <kimshun1227@icloud.com>
2024-08-12 02:38:10 +0900

    Delete micropost

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,10 +1,16 @@
 class MicropostsController < ApplicationController
   before_action :logged_in_user, only: %i[create destroy]
+  before_action :correct_user, only: :destroy
 
   private
 
   def micropost_params
     params.require(:micropost).permit(:content)
+  end
+
+  def correct_user
+    @micropost = current_user.microposts.find_by(id: params[:id])
+    redirect_to(root_url) if @micropost.nil?
   end
 
   public
@@ -20,5 +26,9 @@ class MicropostsController < ApplicationController
     end
   end
 
-  def destroy; end
+  def destroy
+    @micropost.destroy
+    flash[:success] = 'Micropost deleted'
+    redirect_back_or_to(root_url, status: :see_other)
+  end
 end
