be6d96620618579b585294975a3eef6af2449eeb
shunsei <kimshun1227@icloud.com>
2024-08-12 00:35:03 +0900

    Restrict micropost access

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,15 @@ class ApplicationController < ActionController::Base
   # どのコントローラからでもログイン関連のメソッドを
   # 呼び出せるようにする
   include SessionsHelper
+
+  private
+
+  # ユーザーのログインを確認する
+  def logged_in_user
+    return if logged_in?
+
+    store_location
+    flash[:danger] = 'Please log in.'
+    redirect_to(login_url, status: :see_other)
+  end
 end
