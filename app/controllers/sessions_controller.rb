43358520829bdcf4c10337587a4e32dd6234778c
shunsei <kimshun1227@icloud.com>
2024-08-11 13:00:59 +0900

    Define activation controller edit action

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,20 +1,26 @@
 class SessionsController < ApplicationController
   def new; end
 
-  # rubocop:disable Metrics/AbcSize
-  # rubocop:disable Metrics/MethodLength
+  # rubocop:disable Metrics
   def create
     @user = User.find_by(email: params[:session][:email].downcase)
     if @user&.authenticate(params[:session][:password])
-      forwarding_url = session[:forwarding_url]
-      # セッション固定を防ぐために
-      # ログインの直前に必ずセッションをリセットする
-      reset_session
-      # ユーザーを記憶する
-      params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
-      # ログイン後にユーザー情報のページにリダイレクトする
-      log_in(@user)
-      redirect_to forwarding_url || @user
+      if @user.activated?
+        forwarding_url = session[:forwarding_url]
+        # セッション固定を防ぐために
+        # ログインの直前に必ずセッションをリセットする
+        reset_session
+        # ユーザーを記憶する
+        params[:session][:remember_me] == '1' ? remember(@user) : forget(@user)
+        # ログイン後にユーザー情報のページにリダイレクトする
+        log_in(@user)
+        redirect_to forwarding_url || @user
+      else
+        message  = 'Account not activated. '
+        message += 'Check your email for the activation link.'
+        flash[:warning] = message
+        redirect_to root_url
+      end
     else
       # エラーメッセージを作成する
       flash.now[:danger] = 'Invalid email/password combination'
