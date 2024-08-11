c01e7a2d7e87dc680b2c012c4e3cac720bc4cd14
shunsei <kimshun1227@icloud.com>
2024-08-11 13:58:17 +0900

    Edit production mailer setting

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,6 @@
 require 'active_support/core_ext/integer/time'
 
+# rubocop:disable Metrics
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
@@ -66,7 +67,18 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  host = 'https://sample-app-kx2c.onrender.com'
+  config.action_mailer.default_url_options = { host: }
+  ActionMailer::Base.smtp_settings = {
+    port: 587,
+    address: 'smtp.mailgun.org',
+    user_name: ENV['MAILGUN_SMTP_LOGIN'],
+    password: ENV['MAILGUN_SMTP_PASSWORD'],
+    domain: host,
+    authentication: :plain
+  }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
