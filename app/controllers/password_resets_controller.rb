01f19269753816adc95d0db48f3b89c540d43019
shunsei <kimshun1227@icloud.com>
2024-08-11 16:24:28 +0900

    Define password reset controller edit action

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,4 +1,21 @@
 class PasswordResetsController < ApplicationController
+  before_action :get_user, only: %i[edit update]
+  before_action :valid_user, only: %i[edit update]
+
+  private
+
+  def get_user
+    @user = User.find_by(email: params[:email])
+  end
+
+  def valid_user
+    return if @user&.activated? && @user&.authenticated?(:reset, params[:id])
+
+    redirect_to root_url
+  end
+
+  public
+
   def new; end
 
   def create
