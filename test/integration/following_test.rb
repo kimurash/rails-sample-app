52fd4e0de8cc6848d5e58320dc7cfff4cefa3e98
shunsei <kimshun1227@icloud.com>
2024-08-12 15:56:04 +0900

    Test follow

--- test/integration/following_test.rb
+++ test/integration/following_test.rb
@@ -1,8 +1,17 @@
 require 'test_helper'
 
-class FollowingTest < ActionDispatch::IntegrationTest
+class Following < ActionDispatch::IntegrationTest
   def setup
-    @user = users(:michael)
+    @user  = users(:michael)
+    @other = users(:archer)
+    log_in_as(@user)
+  end
+end
+
+class FollowingTest < Following
+  def setup
+    @user  = users(:michael)
+    @other = users(:archer)
     log_in_as(@user)
   end
 
@@ -22,6 +31,46 @@ class FollowingTest < ActionDispatch::IntegrationTest
     assert_match @user.followers.count.to_s, response.body
     @user.followers.each do |user|
       assert_select 'a[href=?]', user_path(user), count: 3
+    end
+  end
+end
+
+class FollowTest < Following
+  test 'should follow a user the standard way' do
+    assert_difference '@user.following.count', 1 do
+      post relationships_path, params: { followed_id: @other.id }
+    end
+    assert_redirected_to @other
+  end
+
+  test 'should follow a user with Hotwire' do
+    assert_difference '@user.following.count', 1 do
+      post relationships_path(format: :turbo_stream),
+           params: { followed_id: @other.id }
+    end
+  end
+end
+
+class Unfollow < Following
+  def setup
+    super
+    @user.follow(@other)
+    @relationship = @user.active_relationships.find_by(followed_id: @other.id)
+  end
+end
+
+class UnfollowTest < Unfollow
+  test 'should unfollow a user the standard way' do
+    assert_difference '@user.following.count', -1 do
+      delete relationship_path(@relationship)
+    end
+    assert_response :see_other
+    assert_redirected_to @other
+  end
+
+  test 'should unfollow a user with Hotwire' do
+    assert_difference '@user.following.count', -1 do
+      delete relationship_path(@relationship, format: :turbo_stream)
     end
   end
 end
